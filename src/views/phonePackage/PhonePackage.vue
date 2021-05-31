e1a27d14681a34ee6d53e8562cb6e2da0e64bd6d
saltedfish <huyashangding@163.com>
2021-05-31 20:33:28 +0800

    commission-2

--- src/views/phonePackage/PhonePackage.vue
+++ src/views/phonePackage/PhonePackage.vue
@@ -14,6 +14,9 @@
       <van-cell class="money" title="佣金" icon="refund-o" :value="calcNOCommission(item) + '元'"></van-cell>
       <van-divider :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }"></van-divider>
     </div>
+    <div>
+      <van-cell title="总计" :value="total"></van-cell>
+    </div>
   </div>
 </template>
 
@@ -48,40 +51,70 @@ export default {
     calcCommission (item) {
       switch (item.productName) {
         case '畅爽冰激凌5G套餐129元':
+          this.total = this.total + calcCommission(item.hbCount, 645, 774, 903)
           return calcCommission(item.hbCount, 645, 774, 903)
         case '畅爽冰激凌5G套餐159元-优化版':
+          this.total = this.total + calcCommission(item.hbCount, 795, 954, 1113)
           return calcCommission(item.hbCount, 795, 954, 1113)
         case '畅爽冰激凌5G套餐199元（极速）':
+          this.total = this.total + calcCommission(item.hbCount, 995, 1194, 1393)
           return calcCommission(item.hbCount, 995, 1194, 1393)
         case '畅爽冰激凌5G套餐239元':
+          this.total = this.total + calcCommission(item.hbCount, 1195, 1434, 1637)
+
           return calcCommission(item.hbCount, 1195, 1434, 1637)
         case '智慧沃家5G极享三千兆套餐-239元档':
+          this.total = this.total + calcCommission(item.hbCount, 1195, 1434, 1673)
+
           return calcCommission(item.hbCount, 1195, 1434, 1673)
         case '畅爽冰激凌5G套餐299元':
+          this.total = this.total + calcCommission(item.hbCount, 1495, 1794, 2093)
+
           return calcCommission(item.hbCount, 1495, 1794, 2093)
         case '5G畅爽冰激凌套餐399元':
+          this.total = this.total + calcCommission(item.hbCount, 1795, 2094, 2393)
+
           return calcCommission(item.hbCount, 1795, 2094, 2393)
         case '畅爽冰激凌5G套餐999元':
+          this.total = this.total + calcCommission(item.hbCount, 3595, 3894, 4193)
           return calcCommission(item.hbCount, 3595, 3894, 4193)
+        default:
+          return 0
       }
     },
     calcNOCommission (item) {
       switch (item.productName) {
         case '畅爽冰激凌5G套餐129元':
+          this.total = this.total + calcCommission(item.dkCount, 620, 749, 878)
+
           return calcCommission(item.dkCount, 620, 749, 878)
         case '畅爽冰激凌5G套餐159元-优化版':
+          this.total = this.total + calcCommission(item.dkCount, 770, 929, 1088)
+
           return calcCommission(item.dkCount, 770, 929, 1088)
         case '畅爽冰激凌5G套餐199元（极速）':
+          this.total = this.total + calcCommission(item.dkCount, 970, 1169, 1368)
+
           return calcCommission(item.dkCount, 970, 1169, 1368)
         case '畅爽冰激凌5G套餐239元':
+          this.total = this.total + calcCommission(item.dkCount, 1170, 1409, 1648)
+
           return calcCommission(item.dkCount, 1170, 1409, 1648)
         case '智慧沃家5G极享三千兆套餐-239元档':
+          this.total = this.total + calcCommission(item.dkCount, 1170, 1409, 1648)
+
           return calcCommission(item.dkCount, 1170, 1409, 1648)
         case '畅爽冰激凌5G套餐299元':
+          this.total = this.total + calcCommission(item.dkCount, 1470, 1769, 2068)
+
           return calcCommission(item.dkCount, 1470, 1769, 2068)
         case '5G畅爽冰激凌套餐399元':
+          this.total = this.total + calcCommission(item.dkCount, 1770, 2069, 2368)
+
           return calcCommission(item.dkCount, 1770, 2069, 2368)
         case '畅爽冰激凌5G套餐999元':
+          this.total = this.total + calcCommission(item.dkCount, 3570, 3869, 4168)
+
           return calcCommission(item.dkCount, 3570, 3869, 4168)
         default:
           return 0
